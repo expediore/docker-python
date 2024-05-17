3bbf2c58ec4c83d505339e5c9cebf72647301dd7
expediore <wsnow9999@gmail.com>
2024-05-17 21:36:32 +0900

    last3

--- app.py
+++ app.py
@@ -352,4 +352,5 @@ def run_crawl(url):
         driver.quit()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)

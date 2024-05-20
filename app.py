459fe44e9f84276d3b559435dc35b1335f19cdbb
expediore <wsnow9999@gmail.com>
2024-05-20 15:53:24 +0900

    last7

--- app.py
+++ app.py
@@ -95,7 +95,6 @@ def run_crawl(url):
     options.add_argument(f'user-agent={user_agent}')
     options.add_argument('--incognito')
     options.add_experimental_option('detach', True)
-    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
